c6bce8cd064d0bcb62c89a6bf94cc7d651f5e5bc
madang11 <madang@s.sfusd.edu>
2020-01-14 02:24:14 +0000

    mai

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-let songName = ["cat"];
-songName.forEach(function(name){
-    $(".songName").append(name)
-});
+let songName = [" "];
+let songArtist = [" "];
+let songLength = [" "];
+let image = [" "];
+let link = [" "];
 
-let songArtist = ["cat"];
-songArtist.forEach(function(artist){
-    $(".songArtist").append(artist)
-});
+$(".button").click(function(){
+    let newSong = $(".songNames").val();
+    songName.push(newSong);
+    $(".songName").empty();
+    songName.forEach(function(name){
+        $(".songName").append(name + " ");
+    });
 
-let songLength = ["cat"];
-songArtist.forEach(function(length){
-    $(".songLength").append(length)
-});
+    let newArtist = $(".songArtists").val();
+    songArtist.push(newArtist);
+    $(".songArtist").empty();
+    songArtist.forEach(function(artist){
+        $(".songArtist").append(artist + " ")
+    });
+    let newList = $(".songLists").val();
+    songList.push(newList);
+    songList.forEach(function(length){
+        $(".songLength").append(length + " ")
+    });
 
-let image = ["https://placekitten.com/g/100/250"];
-image.forEach(function(imagee){
-    $(".image").append(`<img src = " ${imagee} ">`)
-});
+    image.forEach(function(imagee){
+        $(".image").append(`<img src = " ${imagee} ">`)
+    });
 
-let link = ["cat"];
-link.forEach(function(linkk){
-    $(".link").wrap(`<a href= " ${linkk} "> Link </a>`);
+    link.forEach(function(linkk){
+        $(".link").wrap(`<a href= " ${linkk} "> Link </a>`);
+    });
 });
