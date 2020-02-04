aa3618dc6be32f93b91f31a713dfb324c4c97c05
madang11 <madang@s.sfusd.edu>
2020-02-04 02:25:19 +0000

    no

--- js/script.js
+++ js/script.js
@@ -1,10 +1,50 @@
-let song = {
-    name: "roxanne"
+let roxanne = {
+    name: "roxanne",
+    artist: "Arizona Zervas",
+    length: "3:20",
+    image: "",
+    Link: ""}
+let songs = [roxanne]
     
-}
+
+$(".button").click(function(){
+    let newSong = $(".songNames").val();
+    let newArtist = $(".songArtists").val();
+    let newLength = $(".songLengths").val();
+    let newImage = $(".songImages").val();
+    let newLink = $(".songLinks").val();
+
+    $('')
+
+    songs.forEach(function(song){
+        $(".songName").append("<li>" + song.name + "</li>")
+        $(".songArtist").append("<li>" + song.artist + "</li>")
+        $(".songLength").append("<li>" + song.length + "</li>")
+        $(".songImages").append("<li>" + song.image + "</li>")
+        $(".songLinks").append("<li>" + song.link + "</li>")
 
 
-/*let songName = [" "];
+
+})
+
+/* let newSong = $(".songNames").val();
+    let newArtist = $(".songArtists").val();
+    let newLength = $(".songLengths").val();
+    let newImage = $(".songImages").val();
+    let newLink = $(".songLinks").val();
+    song.name = song['name'] + newSong
+    $(".songName").empty();
+    $(".songName").append(newSong)
+
+    song.artist = song['artist'] + newSong
+    $(".songArtist").empty();
+    $(".songArtist").append(song.name)
+v
+    song.length = song['length'] + newLength
+    song.image = song['image'] + newImage
+    song.songLink = song['songLink'] + newLink
+ });
+let songName = [" "];
 let songArtist = [" "];
 let songLength = [" "];
 let songImage = [" "];
@@ -40,4 +80,7 @@ $(".button").click(function(){
     songLink.forEach(function(link){
         $(".songLink").append(`<a href= " ${link} "> Link </a>`);
     });
+    */
 });
+
+
